67e48d04ff95df14508b6903573cc8fab4fbcd10
Amal Bose <axatrikx@gmail.com>
2012-05-19 09:22:12 +0530

    Set up maven directory structure.

---
+++ src/main/java/com/axatrikx/axadriver/AxaDriver.java
@@ -0,0 +1,13 @@
+package com.axatrikx.axadriver;
+
+public class AxaDriver {
+
+	/**
+	 * @param args
+	 */
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		System.out.println("Hello");
+	}
+
+}
