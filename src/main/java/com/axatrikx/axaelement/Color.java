093a154e8f11f8b95920a05fc3fef39d101b29e5
axatrikx <axatrikx@gmail.com>
2012-05-19 17:19:00 +0530

    Added color class

---
+++ src/main/java/com/axatrikx/axaelement/Color.java
@@ -0,0 +1,9 @@
+package com.axatrikx.axaelement;
+
+/**
+ * Color class is responsible for managing color attribute for elements. Since different browsers interpret
+ * color in different ways, this class gives a common way to access color.
+ */
+public class Color {
+
+}
